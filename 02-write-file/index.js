a1bfa3173b7c08217ebd276a4c24d5ac672a9dfd
HoneySafonovets <kfs.boi@yandex.ru>
2025-01-21 21:56:04 +0300

    fix: create file function

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -5,32 +5,34 @@ const { stdin: input, stdout: output } = require('node:process');
 const rl = readline.createInterface({ input, output });
 const filePath = path.join('./02-write-file/text.txt');
 
+fs.exists(filePath, (exists) => {
+  if (exists === false) {
+    fs.createWriteStream(filePath, 'utf-8');
+  }
+});
 
-function writeFile() {
-  const entry = fs.createWriteStream(filePath, 'utf-8');
+// rl.question('Hello User! What do you want to write to the file?')
+console.log('Hello User! What do you want to write to the file?');
+rl.prompt();
 
-  // rl.question('Hello User! What do you want to write to the file?')
-  console.log('Hello User! What do you want to write to the file?');
-  rl.prompt();
-
-  rl.on('line', (line) => {
-    if (line === 'exit') {
-      console.log(`
-        Goodbye!
-      `)
-      rl.close();
-    }
+rl.on('line', (line) => {
+  if (line === 'exit') {
+    console.log(`
+      Goodbye!
+    `)
+    rl.close();
+  } else {
     fs.appendFile(filePath, `  ${line}`, 'utf-8', (err) => {
       if (err) {
         throw err;
       };
     });
-  });
-  rl.on('SIGINT', () => {
-    console.log(`  Goodbye!`);
-    rl.close();
-  });
-};
-writeFile();
+    rl.on('SIGINT', () => {
+      console.log(`  Goodbye!`);
+      rl.close();
+    });
+  }
+});
+
 
 // node 02-write-file
